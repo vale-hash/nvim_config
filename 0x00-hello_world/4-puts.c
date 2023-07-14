084bb0e5b9c2f32f3f95b3c72b22b4f77a809521
Michael Elekwachi <newlechim@gmail.com>
2023-07-14 03:45:19 +0100

    take 6 thanks to betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-        char Mystr[] = """Programming is like building a multilingual puzzle";
-        puts(Mystr);
+    char Mystr[] = "Programming is like building a multilingual puzzle";
+    puts(Mystr);
 
-        return 0;
+    return (0);
 }
+
