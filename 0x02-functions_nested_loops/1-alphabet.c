fcc1b542109b23af3683b53c23e4cd5a3a4a3c3b
Michael Elekwachi <elekwachi50@gmail.com>
2023-07-19 10:43:11 +0100

    0x02 nested loops added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void) {
+  char ch = 97;
+
+  for (ch = 97; ch <= 122; ch++)
+    putchar(ch);
+}
