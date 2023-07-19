be003138a9893700732ff520d4081a80a1454574
Michael Elekwachi <elekwachi50@gmail.com>
2023-07-19 11:53:14 +0100

    i think i understand better

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+#include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
  * Return: Always 0
  */
+void print_alphabet(void);
+
 void print_alphabet(void) {
   char ch = 97;
 
-  for (ch = 97; ch <= 122; ch++)
+  for (ch = 97; ch <= 122; ch++) {
     putchar(ch);
+  }
+  putchar('\n');
 }
